91c0f1057023c7e530c220186b90edb585f30edc
Ícaro Jerry <icaro.jerry@gmail.com>
2015-08-25 08:18:40 -0300

    Adding some examples in C++

---
+++ c++/Fillvector.cpp
@@ -0,0 +1,21 @@
+// Copy an entire file into a vector of string
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    vector <string> v;
+    ifstream in ("Fillvector.cpp"); 
+    string line;
+    while (getline (in, line))
+        v.push_back (line);// Add the line to the end
+ 
+
+    for (int i = 0; i < v.size (); i++)
+    cout << i << ": " << v[i] << endl; // Add line numbers
+}
---
+++ c++/helloword.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello, Word! I am" <<8<<"Today!"<<endl;
+}
